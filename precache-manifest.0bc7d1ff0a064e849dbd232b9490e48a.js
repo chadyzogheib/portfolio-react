f72e2693c95bcfaec310cedaba6db2085bacf104
chadyzogheib <54284150+chadyzogheib@users.noreply.github.com>
2024-08-08 21:09:58 +0000

    Build: (${GITHUB_SHA}) ${GITHUB_EVENT_NAME}

---
+++ precache-manifest.0bc7d1ff0a064e849dbd232b9490e48a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610283d9b8a0a9cef6b9ea13a91259f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcdffacd898e2276cdbb",
+    "url": "/static/css/main.9e56872e.chunk.css"
+  },
+  {
+    "revision": "f34b043eb9054ce55cfe",
+    "url": "/static/js/2.dde60278.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.dde60278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d73de3b3f2f909f53b",
+    "url": "/static/js/3.766107e3.chunk.js"
+  },
+  {
+    "revision": "bcdffacd898e2276cdbb",
+    "url": "/static/js/main.f6840389.chunk.js"
+  },
+  {
+    "revision": "0f39a51405518290a564",
+    "url": "/static/js/runtime-main.06fa3418.js"
+  }
+]);
